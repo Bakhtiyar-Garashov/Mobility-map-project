f06438e6605c76eb599edb56ac5d46d29994a62b
Bakhtiyar-Garashov <qarashov787@gmail.com>
2020-04-16 21:50:13 +0300

    First commit

---
+++ data/point.js
@@ -0,0 +1,11 @@
+data={
+"type": "FeatureCollection",
+"name": "points",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "OBJECTID": 3.0, "FIPS": "AJ", "ISO2": "AZ", "ISO3": "AZE", "UN": 31.0, "NAME": "Azerbaijan", "CentX": 49.864562, "CentY": 40.402352, "EndX": 49.864562, "EndY": 40.402352, "ID1": 31.0, "ID2": 31.0 }, "geometry": { "type": "Point", "coordinates": [ 49.86456, 40.40235 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": 202.0, "FIPS": "TU", "ISO2": "TR", "ISO3": "TUR", "UN": 792.0, "NAME": "Turkey", "CentX": 28.986044, "CentY": 41.053673, "EndX": 49.864562, "EndY": 40.402352, "ID1": 31.0, "ID2": 792.0 }, "geometry": { "type": "Point", "coordinates": [ 28.98604, 41.05367 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": 68.0, "FIPS": "GG", "ISO2": "GE", "ISO3": "GEO", "UN": 268.0, "NAME": "Georgia", "CentX": 44.801763, "CentY": 41.730324, "EndX": 49.864562, "EndY": 40.402352, "ID1": 31.0, "ID2": 268.0 }, "geometry": { "type": "Point", "coordinates": [ 44.80176, 41.73032 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": 53.0, "FIPS": "EN", "ISO2": "EE", "ISO3": "EST", "UN": 233.0, "NAME": "Estonia", "CentX": 25.525771, "CentY": 58.674008, "EndX": 49.864562, "EndY": 40.402352, "ID1": 31.0, "ID2": 233.0 }, "geometry": { "type": "Point", "coordinates": [ 25.52577, 58.67401 ] } }
+]
+}
